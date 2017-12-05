7b16e9edc43f718a1a44bf655af18c88bb67d00f
Kirill <kirivasile@yandex.ru>
2017-12-05 16:36:22 +0300

    Build commands created

---
+++ build.sh
@@ -0,0 +1,4 @@
+echo "Debug lint HTML report will be written to app\build\reports\lint-results.html"
+echo "Debug test HTML report will be written to app\build\reports\tests\testDebugUnitTest\index.html"
+echo "Debug code coverage HTML report will be written to app\build\reports\jacoco\jacocoTestDebugUnitTestReport\html\index.html"
+gradlew clean build jacocoTestReport
